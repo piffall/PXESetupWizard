5e6f66a0598a7f8e69a4d840b327cad2d0146a79
Cristòfol Torrens <piffall@gmail.com>
2014-03-08 00:53:51 +0100

    First release tag

---
+++ scripts/exec/charmeleon.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Changing to working directory
+cd $(dirname "$0")
+. ../config.sh
+
+# Temp paths and files
+export SRC_PKG="http://chameleon.osx86.hu/file_download/42/Chameleon-2.1-r2069.iso.zip"
+export TRG_NME="charmeleon"
+export TRG_PKG=$TRG_NME".zip"
+
+# Download and deploy it
+. ./common/download.sh
+wget -O $TMP/$TRG_PKG $SRC_PKG --header="Referer: http://chameleon.osx86.hu/static/some-words-about-donation?ref=file_download/42/Chameleon-2.1-r2069.iso.zip"
+unzip $TMP/$TRG_PKG -d $TFTP_PATH/boot/$TRG_NME
+. ./common/clean.sh
