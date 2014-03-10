0eb454bff7064c5fd99dba542893b5f971f94e9e
Cristòfol Torrens <tofol@scip.es>
2014-03-10 15:50:41 +0100

    FreeDOS config moved and ReactOS added

---
+++ scripts/exec/reactos.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Changing to working directory
+cd $(dirname "$0")
+. ../config.sh
+
+# Temp paths and files
+export SRC_PKG="http://downloads.sourceforge.net/project/reactos/ReactOS/0.3.16/ReactOS-0.3.16-REL-live.zip"
+export TRG_NME="reactos"
+export TRG_PKG=$TRG_NME".zip"
+
+# Download and deploy it
+. ./common/download.sh
+unzip $TMP/$TRG_PKG -d $TFTP_PATH/boot/$TRG_NME
+. ./common/clean.sh
