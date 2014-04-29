3e1c90e47e9d5686b91c9c36333fe855802ca1d2
Cristòfol Torrens <tofol@scip.es>
2014-04-29 14:40:36 +0200

    KALI installer added

---
+++ scripts/exec/kali-amd64.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Changing to working directory
+cd $(dirname "$0")
+. ../config.sh
+
+# Temp paths and files
+export SRC_PKG="http://repo.kali.org/kali/dists/kali/main/installer-amd64/current/images/netboot/netboot.tar.gz"
+export TRG_NME="kali"
+export TRG_PKG=$TRG_NME".tar.gz"
+
+# Download and deploy it
+. ./common/download.sh
+[ ! -d $TFTP_PATH/boot/$TRG_NME ] && mkdir $TFTP_PATH/boot/$TRG_NME
+tar vxzf $TMP/$TRG_PKG -C $TFTP_PATH/boot/$TRG_NME
+. ./common/clean.sh
