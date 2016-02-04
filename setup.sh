93e17cd549f94d34a8087d2757a81fd445be7269
Cristòfol Torrens <piffall@gmail.com>
2016-02-04 22:05:45 +0100

    IP selector

--- setup.sh
+++ setup.sh
@@ -1,44 +1,73 @@
 #!/usr/bin/env bash
 
+# ...
 echo "+------------------------------------+"
 echo "|          PXE SETUP WIZARD          |"
 echo "+------------------------------------+"
+echo ""
 
 cd $(dirname "$0") 
 export TFTP_PATH=$(pwd)
 export SCRIPT_PATH=$TFTP_PATH/scripts/exec
 
-DEF_IP=$(ip addr list eth0 | grep "inet " | cut -d' ' -f6 | cut -d/ -f1)
-echo "TFTP Server IP address: [$DEF_IP]"
-read IP
-[ -z "$IP" ] && IP=$DEF_IP
+# Parse IPs, set default and calc length
+IPS=($(ip addr | grep 'inet ' | cut -d' ' -f6 | cut -d '/' -f1))
+DEF_IP_INDEX=1
+IPS_LEN=${#IPS[*]}
 
+# Header of IP choice 
+echo "Select an IP address to bind TFTP service"
+echo "-----------------------------------------"
+echo ""
+
+for (( i=0; i<=$(( $IPS_LEN -1 )); i++ ))
+do
+    if [ $i -eq $DEF_IP_INDEX ]; then
+        echo -n " * "
+    else
+        echo -n "   "
+    fi
+    echo "$i. ${IPS[$i]}"
+done
+
+# Ask user to choose one of the IP addresses
+echo ""
+echo -n "Press enter to keep the current choice[*], or type selection number: "
+read IP_INDEX
+
+if [ "x$IP_INDEX" == "x" ]; then
+    IP=${IPS[$DEF_IP_INDEX]}
+else
+    IP=${IPS[$IP_INDEX]}
+fi
+
+# Inject IP address
 cd conf
 BLD_PATH=$(pwd)
 for FILE in $(ls src/)
 do
-	cat $BLD_PATH/src/$FILE | sed -s 's/{ip}/'$IP'/g' > $BLD_PATH/available/$FILE
+    cat $BLD_PATH/src/$FILE | sed -s 's/{ip}/'$IP'/g' > $BLD_PATH/available/$FILE
 done
 
+# Create all symlinks
 . $TFTP_PATH/scripts/enable-all.sh
 
-DOWNLOAD="N"
-echo "Do you want to run scripts? [N/y]"
+# Ask to run scripts
+DOWNLOAD="y"
+echo -n "Do you want to run scripts? [Y/n]: "
 read DOWNLOAD
 DOWNLOAD=$(echo $DOWNLOAD | tr '[:upper:]' '[:lower:]')
+echo ""
 
+# Enter path and run each installation script
 cd $TFTP_PATH
 if [ "$DOWNLOAD" = "y" ]; then
-	for SCRIPT in $(find $SCRIPT_PATH -executable -type f | grep -v "sudo" | sort)
-	do
-		echo -e "\x1b[30;01m""Running script "$SCRIPT"\x1b[39;49;00m"
-		$SCRIPT
-	done
-	for SCRIPT in $(find $SCRIPT_PATH -executable -type f | grep "sudo" | sort)
-	do
-		echo -e "\x1b[31;01m""Running script "$SCRIPT "as root \x1b[39;49;00m"
-		sudo $SCRIPT
-	done
+    for SCRIPT in $(find $SCRIPT_PATH -executable -type f | sort)
+    do
+        echo -e "\x1b[30;01m""Running script "$SCRIPT"\x1b[39;49;00m"
+        $SCRIPT
+    done
 fi
 
+# Fix bad vesamenu files
 . ./fix_vesamenu.sh
