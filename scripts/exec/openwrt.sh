2403c06a3ea8bef4443bc503e85e5e18b357288d
Cristòfol Torrens <piffall@gmail.com>
2016-05-18 19:01:43 +0200

    OpenWRT updated

--- scripts/exec/openwrt.sh
+++ scripts/exec/openwrt.sh
@@ -6,7 +6,7 @@ source ../config.sh
 
 # Temp paths and files
 OPENWRT_VNAME="chaos_calmer"
-OPENWRT_VNUM="15.05"
+OPENWRT_VNUM="15.05.1"
 export SRC_PKG="http://downloads.openwrt.org/${OPENWRT_VNAME}/${OPENWRT_VNUM}/x86/64/openwrt-${OPENWRT_VNUM}-x86-64-combined-squashfs.img"
 export TRG_NME="openwrt"
 export TRG_PKG=$TRG_NME".img"
