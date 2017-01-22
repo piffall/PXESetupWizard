e5af9e8af7ba49dc16eb82fd65cb3fe212688414
Cristòfol Torrens <piffall@gmail.com>
2017-01-22 22:15:32 +0100

    Limit wget limit to 2 tries

--- scripts/exec/common/download.sh
+++ scripts/exec/common/download.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 [ ! -f $TMP/$TRG_PKG ] && {
     echo "wget $SRC_PKG -O $TMP/$TRG_PKG"
-    wget $SRC_PKG -O $TMP/$TRG_PKG
+    wget --tries=2 $SRC_PKG -O $TMP/$TRG_PKG
 }
 
 [ ! -f $TMP/$TRG_PKG2 ] && [ "X" != "X$TRG_PKG2" ] && {
     echo "wget $SRC_PKG2 -O $TMP/$TRG_PKG2"
-    wget $SRC_PKG2 -O $TMP/$TRG_PKG2
+    wget --tries=2 $SRC_PKG2 -O $TMP/$TRG_PKG2
 }
