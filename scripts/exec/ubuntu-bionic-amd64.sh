d63f039144f8ea91b52138a3ba2b2c9cdc5c3dbc
Cristòfol Torrens <piffall@gmail.com>
2023-04-05 17:54:33 +0200

    Removed Ubuntu 18.04, added Ubuntu 22.04

--- scripts/exec/ubuntu-bionic-amd64.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Changing to working directory
-cd $(dirname "$0")
-. ../config.sh
-
-export REL="bionic"
-export ARCH="amd64"
-. ./ubuntu/ubuntu-common.sh
--- scripts/exec/ubuntu-bionic-i386.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Changing to working directory
-cd $(dirname "$0")
-. ../config.sh
-
-export REL="bionic"
-export ARCH="i386"
-. ./ubuntu/ubuntu-common.sh
